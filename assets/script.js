d178960807c97910081de68d1a76f5aa6b229dbe
crosgar <cecilia.rossi.garcia@gmail.com>
2021-04-08 15:40:13 -0700

    created array for lower case letters

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// Assignment code here
+var alphabetLower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var generatePassword = function() {
+
+
+};
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
