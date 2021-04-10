49b144b91f6a6d765e201a1331293ba61b5ed21b
crosgar <cecilia.rossi.garcia@gmail.com>
2021-04-09 21:30:43 -0700

    refactored code, added error alert for invalid inputs

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,8 @@
 
-let specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
+let specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-let lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-let upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+let lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 // Get references to the #generate element
@@ -102,39 +21,52 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  let pot = [];
+  let userSelect = [];
 
   let finalPw = [];
 
-  // capture user inputs
-  let length = parseInt(prompt("Enter a number between 8 and 128 to determine the length of your password:"));
-  let shouldIncludeLower = confirm("Do you want lower case letters in your password?");
-  let shouldIncludeUpper = confirm("Do you want lower case letters in your password?");
-  let shouldIncludeNumbers = confirm("Do you want lower case letters in your password?");
-  let shouldIncludeSpecialChar = confirm("Do you want lower case letters in your password?");
+  var length = parseInt(prompt("Enter a number between 8 and 128 to determine the length of your password:"));
 
-  // fill the pot
+  if (length > 128 || length < 8 || length === null || length === "" || !length) {
+    alert(length + " is not a valid answer, please try again!");
+  }
+
+  else {
+    var userPassword = otherQuestions(userSelect, finalPw, length);
+    console.log(`userPassword = ${userPassword}`);
+    return userPassword;
+  }
+
+};
+
+function otherQuestions(userSelect, finalPw, length) {
+  let shouldIncludeLower = confirm("Do you want lower case letters in your password?");
+  let shouldIncludeUpper = confirm("Do you want upper case letters in your password?");
+  let shouldIncludeNumbers = confirm("Do you want numbers in your password?");
+  let shouldIncludeSpecialChar = confirm("Do you want special characters in your password?");
+
   if (shouldIncludeLower) {
-    pot = pot.concat(lowerCasedCharacters);
+    userSelect = userSelect.concat(lowerCasedCharacters);
   }
 
   if (shouldIncludeUpper) {
-    pot = pot.concat(upperCasedCharacters);
+    userSelect = userSelect.concat(upperCasedCharacters);
   }
 
   if (shouldIncludeNumbers) {
-    pot = pot.concat(numericCharacters);
+    userSelect = userSelect.concat(numericCharacters);
   }
   if (shouldIncludeSpecialChar) {
-    pot = pot.concat(specialCharacters);
+    userSelect = userSelect.concat(specialCharacters);
   }
 
-  // randomly pick from the pot
   for (let i = 0; i < length; i++) {
-    let index = Math.floor(Math.random() * pot.length - 1);
-    let temp = pot[index];
+    let index = Math.floor(Math.random() * userSelect.length - 1);
+    let temp = userSelect[index];
     finalPw.push(temp);
+    console.log(`length = ${length}`);
   }
-return finalPw.join('');
+  console.log(`finalPw = ${finalPw}`);
+  return finalPw.join('');
 
 };
